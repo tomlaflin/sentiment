4ad805df04c17fc6f60375152fd1160eff299c2b
tomlaflin <tom.laflin@gmail.com>
2024-01-05 15:17:27 -0600

    Implement extremely basic and ugly display of a character's attributes and allow creation of new ones on the character sheet.

--- sheets/character-sheet.mjs
+++ sheets/character-sheet.mjs
@@ -12,10 +12,63 @@ export default class CharacterSheet extends ActorSheet {
     /** @inheritdoc */
     async getData(options) {
         const context = await super.getData(options);
+
+        await this.#populateDescription(context);
+        this.#populateAttributes(context);
+
+        return context;
+    }
+
+    /**
+    * Transform the raw description property into enriched HTML and embed it into the context for easy access.
+    * @param context
+    * @private
+    */
+    async #populateDescription(context) {
         context.descriptionHTML = await TextEditor.enrichHTML(context.data.system.description, {
             secrets: this.document.isOwner,
             async: true
         });
-        return context;
+    }
+
+    /**
+    * Iterate all owned items and embed a collection containing only the attributes into the context for easy access.
+    * @param context
+    * @private
+    */
+    #populateAttributes(context) {
+        context.attributes = [];
+        for (let item of context.items) {
+            if (item.type == "attribute") {
+                context.attributes.push(item);
+            }
+        }
+    }
+
+    /** @inheritdoc */
+    activateListeners(html) {
+        super.activateListeners(html);
+        
+        if (!this.isEditable) {
+            return;
+        }
+        
+        html.find(".attribute-control").click(this.#onAttributeControlClick.bind(this));
+    }
+
+    /**
+    * Handle event when the user clicks an Attribute Control
+    * @param event
+    * @private
+    */
+    async #onAttributeControlClick(event) {
+        event.preventDefault();
+        
+        const itemData = {
+            name: "New Attribute",
+            type: "attribute",
+        };
+        
+        return await Item.create(itemData, { parent: this.actor });
     }
 }
