3f83264c3916be78d3f9b371a03a9ac023142a0e
Tom <Tom@TERRANOVA>
2024-01-15 13:00:03 -0600

    Add character's health, speed, and experience to the character sheet header.

--- documents/character.mjs
+++ documents/character.mjs
@@ -2,6 +2,30 @@ export default class CharacterData extends foundry.abstract.DataModel {
     static defineSchema() {
         return {
             description: new foundry.data.fields.HTMLField(),
+            health: new foundry.data.fields.SchemaField({
+                value: new foundry.data.fields.NumberField({
+                    integer: true,
+                    initial: 10
+                }),
+                min: new foundry.data.fields.NumberField({
+                    integer: true,
+                    initial: 0
+                }),
+                max: new foundry.data.fields.NumberField({
+                    integer: true,
+                    initial: 10
+                })
+            }),
+            speed: new foundry.data.fields.NumberField({
+                integer: true,
+                min: 0,
+                initial: 30,
+            }),
+            experience: new foundry.data.fields.NumberField({
+                integer: true,
+                min: 0,
+                initial: 0,
+            }),
             swing: new foundry.data.fields.SchemaField({
                 attributeId: new foundry.data.fields.StringField({
                     nullable: true,
