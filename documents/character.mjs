tomlaflin/sentiment

commit 34baf1cd325f1849dc68e12889b45d0cc8c9e541

Update Character interface to have a single method for setting attribute status. This removes a little duplication but is mainly to simplify the interface for calling from the token hud module.

documents/character.mjs
@@ -454,36 +454,15 @@ export class Character extends Actor {
     }
 
     /**
-    * Restore the specified attribute.
+    * Set the status of the specified attribute.
     * @param attributeId
+    * @param status
     */
-    restoreAttribute(attributeId) {
+    setAttributeStatus(attributeId, status) {
         const attribute = this.items.find((item) => item._id === attributeId);
-        attribute.update({ "system.status": AttributeStatus.Normal });
-    }
+        attribute.update({ "system.status": status });
 
-    /**
-    * Lock out the specified attribute.
-    * @param attributeId
-    */
-    lockOutAttribute(attributeId) {
-        const attribute = this.items.find((item) => item._id === attributeId);
-        attribute.update({ "system.status": AttributeStatus.LockedOut });
-
-        if (attributeId === this.system.swing.attributeId) {
-            this.dropSwing();
-        }
-    }
-
-    /**
-    * Wound the specified attribute.
-    * @param attributeId
-    */
-    woundAttribute(attributeId) {
-        const attribute = this.items.find((item) => item._id === attributeId);
-        attribute.update({ "system.status": AttributeStatus.Wounded });
-
-        if (attributeId === this.system.swing.attributeId) {
+        if (status !== AttributeStatus.Normal) {
             this.dropSwing();
         }
     }

sheets/character-sheet.mjs
@@ -240,7 +240,7 @@ export default class CharacterSheet extends ActorSheet {
         event.preventDefault();
 
         const attribute = this.#getItemFromListEvent(event);
-        this.object.restoreAttribute(attribute._id);
+        this.object.setAttributeStatus(attribute._id, AttributeStatus.Normal);
     }
 
     /**
@@ -252,7 +252,7 @@ export default class CharacterSheet extends ActorSheet {
         event.preventDefault();
         
         const attribute = this.#getItemFromListEvent(event);
-        this.object.lockOutAttribute(attribute._id);
+        this.object.setAttributeStatus(attribute._id, AttributeStatus.LockedOut);
     }
 
     /**
@@ -264,7 +264,7 @@ export default class CharacterSheet extends ActorSheet {
         event.preventDefault();
 
         const attribute = this.#getItemFromListEvent(event);
-        this.object.woundAttribute(attribute._id);
+        this.object.setAttributeStatus(attribute._id, AttributeStatus.Wounded);
     }
 
     /**

commit 5bfd364b9618d24720ddad826afade5817b75587

Fix error introduced in commit 34baf1cd - we lost the check of swing attribute ID when deciding whether to call dropSwing upon an attribute being locked out or wounded.

documents/character.mjs
@@ -474,7 +474,7 @@ export class Character extends Actor {
         const attribute = this.items.find((item) => item._id === attributeId);
         attribute.update({ "system.status": status });
 
-        if (status !== AttributeStatus.Normal) {
+        if (attributeId === this.system.swing.attributeId && status !== AttributeStatus.Normal) {
             this.dropSwing();
         }
     }